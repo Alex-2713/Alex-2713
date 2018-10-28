d4c90c0a7578c7f59af3957ee21370259588f4fe
Alex-2713 <Alex_2713_Office@163.com>
2018-10-28 12:33:27 +0800

    Init Upload

---
+++ Alex Parent/Alex Core/src/main/java/info/alex/core/AlexCoreApplication.java
@@ -0,0 +1,16 @@
+package info.alex.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Spring Boot 项目启动程序
+ * @author Alex
+ */
+@SpringBootApplication
+public class AlexCoreApplication {
+    public static void main(String[] args) {
+        // SpringApplication启动
+        SpringApplication.run(AlexCoreApplication.class, args);
+    }
+}
